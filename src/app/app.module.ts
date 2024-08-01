e76adad48c561c983ed6ebc1bf0e10f71eea8add
Ghariani Islem <islem24762048@gmail.com>
2024-08-01 18:37:35 +0100

    completing about asrction,developping service section,adding some features to the header section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServiceComponent } from './components/service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule
