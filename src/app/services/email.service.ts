0d9b0caecc742f0f40a019284e4edb5c13d025b6
Islem Ghariani <islemghariani.dev@gmail.com>
2025-10-20 10:48:44 +0100

    set vercel variables

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import emailjs from '@emailjs/browser';
 
 import { ToastrService } from 'ngx-toastr';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    supabaseapiUrl: process.env['SUPABASE_URL'],
+    supabaseapiKey: process.env['SUPABASE_ANON_KEY'] ,
+    emailJscontact_service: process.env['EMAILJS_SERVICE_ID'],
+    emailJscontact_form: process.env['EMAILJS_TEMPLATE_ID'] ,
+    emailjspublickey: process.env['EMAILJS_PUBLIC_KEY']
+};
